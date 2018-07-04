5e0d975c13f7bf27defedd13951df3bdc84236ec
Vajahath Ahmed <vajuoff.1@gmail.com>
2018-07-04 16:38:50 +0530

    adjesting ports

--- src/routes/swagger.ts
+++ src/routes/swagger.ts
@@ -3,12 +3,12 @@ import swaggerJSDoc = require('swagger-jsdoc');
 const options = {
   swaggerDefinition: {
     info: {
-      title: 'Aumet API Docs',
+      title: 'Miwago API Docs',
       version: '1.0.0',
       description: '',
     },
-    host: 'localhost:4000',
-    basePath: '/api',
+    host: 'localhost:6233',
+    basePath: '/v1',
   },
   apis: ['**/*.docs.yaml'],
 };
