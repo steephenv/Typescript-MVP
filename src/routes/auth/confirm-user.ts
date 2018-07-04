c64c824db9e549b9d0d412ce2a78df0c59bf3df2
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2018-07-04 19:35:10 +0530

    confrim user functionality

---
+++ src/routes/auth/confirm-user.ts
@@ -0,0 +1,58 @@
+import { RequestHandler } from 'express';
+
+import { TempUser } from '../../models/TempUser';
+import { User } from '../../models/User';
+
+import { messages } from '../../config/app/messages';
+import {
+  RequestError,
+  RequestErrorType,
+} from '../../error-handler/RequestError';
+
+export const confirmUser: RequestHandler = async (req, res, next) => {
+  try {
+    const twentyMinutesBefore = new Date();
+    twentyMinutesBefore.setMinutes(twentyMinutesBefore.getMinutes() - 20);
+    const criteria = {
+      token: req.query.token,
+      createdAt: {
+        $gte: new Date(twentyMinutesBefore),
+      },
+    };
+    console.log(criteria);
+    const user: any = await TempUser.findOne(criteria)
+      .lean()
+      .exec();
+    if (!user) {
+      return next(
+        new RequestError(
+          RequestErrorType.UNPROCESSABLE_ENTITY,
+          messages.noUser.ENG,
+        ),
+      );
+    }
+    const extUser = await User.findOne({ email: user.email });
+    if (extUser) {
+      return next(
+        new RequestError(RequestErrorType.CONFLICT, messages.emailExisting.ENG),
+      );
+    }
+    if (!user) {
+      return next(
+        new RequestError(
+          RequestErrorType.UNPROCESSABLE_ENTITY,
+          messages.noUser.ENG,
+        ),
+      );
+    }
+    delete user._id;
+    const newUser = new User(user);
+    const userData = await newUser.save();
+    const removedData = await TempUser.findOneAndRemove({
+      email: req.body.email,
+    });
+    return res.status(201).send({ success: true });
+  } catch (err) {
+    return next(new RequestError(RequestErrorType.INTERNAL_SERVER_ERROR));
+  }
+};
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -3,6 +3,9 @@ import * as express from 'express';
 import { register } from './register';
 import { registerValidation } from './validators/register-rules';
 
+import { confirmUser } from './confirm-user';
+
 export const auth = express.Router();
 
 auth.post('/register', registerValidation, register);
+auth.post('/confirm', confirmUser);
