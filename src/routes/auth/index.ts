212915e98d40e928762a2aecd9c7896eb7d55ca4
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2018-07-05 12:38:42 +0530

    new routes added

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -10,6 +10,9 @@ import { forgotPassword } from './forgot-password';
 
 import { listCompanyDetails } from './list-company-details';
 
+import { emailRecoveryFunction } from './email-recovery';
+import { recoveryEmailRules } from './validators/email-recovery-rules';
+
 export const auth = express.Router();
 
 auth.post('/register', registerValidation, register);
@@ -18,3 +21,5 @@ auth.post('/confirm', confirmUser);
 auth.post('/forgot-password', forgotPasswordRules, forgotPassword);
 
 auth.get('/list-company-details', listCompanyDetails);
+
+auth.post('/email_recovery', recoveryEmailRules, emailRecoveryFunction);
