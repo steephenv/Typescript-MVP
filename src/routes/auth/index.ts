c1d26b97f2a94c4e81355f6fe36fb7d6361aa93d
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2018-07-05 12:15:43 +0530

    new routes added

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -8,9 +8,13 @@ import { confirmUser } from './confirm-user';
 import { forgotPasswordRules } from './validators/forgot-rules';
 import { forgotPassword } from './forgot-password';
 
+import { listCompanyDetails } from './list-company-details';
+
 export const auth = express.Router();
 
 auth.post('/register', registerValidation, register);
 auth.post('/confirm', confirmUser);
 
 auth.post('/forgot-password', forgotPasswordRules, forgotPassword);
+
+auth.get('/list-company-details', listCompanyDetails);
