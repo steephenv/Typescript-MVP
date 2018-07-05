5672b62c56d49a724418a241736c25e7a719e803
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2018-07-05 11:52:47 +0530

    dummy user added

--- src/startup/users.ts
+++ src/startup/users.ts
@@ -1,4 +1,5 @@
 import { User } from '../models/User';
+import { Promise as BluePromise } from 'bluebird';
 
 const consultant = {
   firstName: 'Tony',
@@ -9,8 +10,18 @@ const consultant = {
   password: 'password',
   mobile: '1111111',
 };
+const dumUser1 = {
+  firstName: 'Tony',
+  lastName: 'Stark',
+  appliedRole: 'Consultant',
+  email: 'loki@marvel.com',
+  role: 'Consultant',
+  password: 'password',
+  mobile: '1111111',
+};
 
 export const initUsers = () => {
-  const savable = new User(consultant);
-  return savable.save();
+  const savableConsultant = new User(consultant);
+  const savableDumUser1 = new User(dumUser1);
+  return BluePromise.all([savableConsultant.save(), savableDumUser1.save()]);
 };
