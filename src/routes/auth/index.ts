a1421b135e875557b0a4710ce9a40959eaa575cc
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2018-07-05 11:52:35 +0530

    forgot password route added

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -5,7 +5,12 @@ import { registerValidation } from './validators/register-rules';
 
 import { confirmUser } from './confirm-user';
 
+import { forgotPasswordRules } from './validators/forgot-rules';
+import { forgotPassword } from './forgot-password';
+
 export const auth = express.Router();
 
 auth.post('/register', registerValidation, register);
 auth.post('/confirm', confirmUser);
+
+auth.post('/forgot-password', forgotPasswordRules, forgotPassword);
