84b404e9b7c7ad0a268020a69c64a4aeba6611d4
Steephen <steephen.varghese@cubettech.com>
2018-07-09 19:05:36 +0530

    skills model added

--- src/models/Skills.ts
+++ src/models/Skills.ts
@@ -8,6 +8,10 @@ const SkillsSchema: Schema = new Schema({
   createdAt: {
     type: Date,
   },
+  cluster: {
+    type: String,
+    enum: ['Personal', 'Leadership', 'Enterpreneuship', 'Functional'],
+  },
   category: {
     type: Schema.Types.ObjectId,
     ref: 'SkillCategory',
@@ -21,6 +25,10 @@ const SkillsSchema: Schema = new Schema({
     ref: 'SkillSubCategory',
   },
   skillTitle: {
+    type: String,
+    required: true,
+  },
+  uniqueTitle: {
     type: String,
     required: true,
   },
