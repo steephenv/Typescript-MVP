f402190ce78288a874833eb8191b93ac506e892b
Steephen <steephen.varghese@cubettech.com>
2018-07-12 17:47:51 +0530

    added work life balance functionality and test file

--- src/routes/profile/validators/save-wlb-rule.ts
+++ src/routes/profile/validators/save-wlb-rule.ts
@@ -1,24 +1,25 @@
 import * as Joi from 'joi';
 import { RequestHandler } from 'express';
 
-const wlbSchema = Joi.object().keys({
-  firstName: Joi.string().required(),
+// tslint:disable:variable-name
+const wlbDataSchema = Joi.object().keys({
   annualAvailableCapacity: Joi.string().required(),
+  capriconsAvailableCapacity: Joi.string().required(),
   frequencyOnsiteWork: Joi.string().required(),
   frequencyHomeOfficeWork: Joi.string().required(),
-  location: Joi.string().required(),
+  location: Joi.array().required(),
   workpermit: Joi.string(),
 });
 
 export const saveWLBRule: RequestHandler = (req, res, next) => {
-  Joi.validate(req.body, wlbSchema, { stripUnknown: true }, (err: any) => {
+  Joi.validate(req.body, wlbDataSchema, { stripUnknown: true }, err => {
+    // console.log();
     if (err) {
       return res.status(422).send({
         success: false,
         msg: err,
       });
     }
-    req.body.createdAt = new Date();
     next();
   });
 };
---
+++ tests/profile/save-wlb.spec.ts
@@ -0,0 +1,51 @@
+import * as supertest from 'supertest';
+import { app, mongoose, mongooseConnectionPromise } from '../../src/app';
+
+let token = '';
+let newUserId: string;
+
+afterAll(() => mongooseConnectionPromise.then(() => mongoose.disconnect()));
+
+beforeAll(done => {
+  supertest(app)
+    .post('/v1/auth/login')
+    .set('X-Requested-With', 'XMLHttpRequest')
+    .send({
+      username: 'stark@marvel.com',
+      password: 'password',
+    })
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        throw err;
+      }
+      newUserId = res.body.data._id;
+      token = res.body.accessToken;
+      return done();
+    });
+});
+
+describe('Test for saving wlb data  ===> ', () => {
+  it('Saving wlb details api', done => {
+    supertest(app)
+      .post(`/v1/profile/save-wlb`)
+      .set('X-Requested-With', 'XMLHttpRequest')
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        annualAvailableCapacity: 'dshfvhsdvf',
+        capriconsAvailableCapacity: 'dsbfhjsdfds',
+        frequencyOnsiteWork: 'dgsvgdsvc',
+        frequencyHomeOfficeWork: 'sdhfhjsdvf',
+        location: ['dsfhhjsd', 'sdfbhsd', 'dbfhbd'],
+        workpermit: 'dsfdshj',
+      })
+      .expect(200)
+      .end((err, res) => {
+        // console.log(res.body.msg.details);
+        if (err) {
+          throw err;
+        }
+        return done();
+      });
+  });
+});
