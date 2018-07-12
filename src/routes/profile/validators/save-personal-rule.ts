ca112f5f23523523f258c4fc96a44e0b15e72a25
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-07-12 20:00:00 +0530

    role based validation and spec file updation

--- src/routes/profile/validators/save-personal-rule.ts
+++ src/routes/profile/validators/save-personal-rule.ts
@@ -6,6 +6,7 @@ const PersonalDataSchema = Joi.object().keys({
   firstName: Joi.string().required(),
   middleName: Joi.string().allow(''),
   lastName: Joi.string().required(),
+  role: Joi.string().required(),
   image: Joi.string().allow(''),
   birthDate: Joi.date().required(),
   countryOfBirth: Joi.string().allow(''),
@@ -22,21 +23,40 @@ const PersonalDataSchema = Joi.object().keys({
   mobilePhone: Joi.string().required(),
   primaryEmail: Joi.string().required(),
   secondaryEmail: Joi.string().required(),
-  taxId: Joi.string().required(),
-  vatId: Joi.number().required(),
-  socialInsuranceId: Joi.number().required(),
-  healthInsuranceType: Joi.string().required(),
-  healthInsurance: Joi.string().required(),
-  ibanNo: Joi.string().required(),
-  bicNo: Joi.string().required(),
+  taxId: Joi.string()
+    .when('role', { is: 'Consultant', then: Joi.required() })
+    .when('role', { is: 'Employee', then: Joi.required() }),
+  vatId: Joi.number().when('role', { is: 'Consultant', then: Joi.required() }),
+  socialInsuranceId: Joi.number().when('role', {
+    is: 'Employee',
+    then: Joi.required(),
+  }),
+  healthInsuranceType: Joi.string().when('role', {
+    is: 'Employee',
+    then: Joi.required(),
+  }),
+  healthInsurance: Joi.string().when('role', {
+    is: 'Employee',
+    then: Joi.required(),
+  }),
+  ibanNo: Joi.string()
+    .required()
+    .when('role', { is: 'Consultant', then: Joi.required() })
+    .when('role', { is: 'Employee', then: Joi.required() }),
+  bicNo: Joi.string()
+    .required()
+    .when('role', { is: 'Consultant', then: Joi.required() })
+    .when('role', { is: 'Employee', then: Joi.required() }),
   personalStatement: Joi.string(),
   summary: Joi.string().required(),
   maidenName: Joi.string().allow(''),
 });
 
 export const savePersonalRule: RequestHandler = (req, res, next) => {
+  req.body.role = res.locals.user.role;
   Joi.validate(req.body, PersonalDataSchema, { stripUnknown: true }, err => {
     // console.log();
+    delete req.body.role;
     if (err) {
       return res.status(422).send({
         success: false,
