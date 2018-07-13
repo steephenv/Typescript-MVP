6ebd8d1ed8873a76e7ab00c6b9a6fe21fcfa4ee9
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-07-13 15:41:41 +0530

    get interview date spec and route added

--- src/routes/interview/index.ts
+++ src/routes/interview/index.ts
@@ -4,10 +4,12 @@ import { scheduleInterviewRules } from './validators/schedule-interview-rules';
 
 import { scheduleInterview } from './schedule-interview';
 import { listAvailableDates } from './list-available-dates';
+import { getInterviewDate } from './get-interview-date';
 
 export const interview = express.Router();
 
 interview.get('/list-dates', listAvailableDates);
+interview.get('/get-date', getInterviewDate);
 interview.post(
   '/schedule-interview',
   scheduleInterviewRules,
