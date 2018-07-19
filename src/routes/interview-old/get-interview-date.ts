66158be57a3093e37c305196e54c6cff891958bd
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-07-19 19:06:13 +0530

    renamed folder files

---
+++ src/routes/interview-old/get-interview-date.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from 'express';
+import {
+  RequestError,
+  RequestErrorType,
+} from '../../error-handler/RequestError';
+import { Interview } from '../../models/Interview';
+import '../../models/TimeSlots';
+// import { TimeSlot } from '../../models/TimeSlots';
+
+export const getInterviewDate: RequestHandler = async (req, res, next) => {
+  try {
+    const comingUserId = req.query.userId
+      ? req.query.userId
+      : res.locals.user.userId;
+    const interview = await Interview.findOne({ userId: comingUserId })
+      .populate('slot')
+      .exec();
+    // await TimeSlot.count({});
+    return res.status(200).send({
+      success: true,
+      data: interview,
+    });
+  } catch (err) {
+    return next(new RequestError(RequestErrorType.INTERNAL_SERVER_ERROR, err));
+  }
+};
