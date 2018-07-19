44d851a3cebb65c43ed840233fc28861e28930d1
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-07-19 19:00:37 +0530

    models for new interview scheduling - avalilability calender and interview details saving models

---
+++ src/models/InterviewAvailabilityCalender.ts
@@ -0,0 +1,39 @@
+/* tslint:disable:variable-name */
+
+import { model as mongooseModel, Schema } from 'mongoose';
+import { ObjectId, ObjectID } from 'bson';
+
+const InterviewAvailabilityCalenderSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  slotDayStartingTime: {
+    type: Date,
+    required: true,
+  },
+  startTime: {
+    type: Date,
+    required: true,
+  },
+  endTime: {
+    type: Date,
+    required: true,
+  },
+  booked: {
+    type: Boolean,
+    default: false,
+  },
+  interviewId: {
+    type: Schema.Types.ObjectId,
+    ref: 'InterviewDetails',
+  },
+  createdAt: {
+    type: Date,
+  },
+});
+
+export const InterviewAvailabilityCalender = mongooseModel(
+  'InterviewAvailabilityCalender',
+  InterviewAvailabilityCalenderSchema,
+);
