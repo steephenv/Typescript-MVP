a196300d77b36550a1593c5a70c37ed40c47dc77
Steephen <steephen.varghese@cubettech.com>
2018-07-20 12:00:23 +0530

    added routes for project category fetching

--- src/routes/project/index.ts
+++ src/routes/project/index.ts
@@ -2,15 +2,20 @@ import * as express from 'express';
 
 import { projectRequestRule } from './validators/project-request-rules';
 import { saveProjectRule } from './validators/save-project-rules';
+import { getProjSubCategoryRules } from './validators/get-proj-category-rules';
 
 import { saveProjectRequest } from './project-request';
 import { saveProject } from './save-project';
 import { getProjectCategory } from './get-project-category';
-import { getSubCategory } from './get-project-category';
+import { getProjSubCategory } from './get-project-category';
 
 export const project = express.Router();
 
 project.post('/save-project-request', projectRequestRule, saveProjectRequest);
 project.post('/save-project', saveProjectRule, saveProject);
 project.get('/get-project-category', getProjectCategory);
-project.get('/get-proj-sub-category', getSubCategory);
+project.get(
+  '/get-proj-sub-category',
+  getProjSubCategoryRules,
+  getProjSubCategory,
+);
