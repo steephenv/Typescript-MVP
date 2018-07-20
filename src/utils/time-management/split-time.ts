d4a1d4d78de40a7a03ee28a087b2529730fa386f
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-07-20 16:44:09 +0530

    duration added dynamically

--- src/utils/time-management/split-time.ts
+++ src/utils/time-management/split-time.ts
@@ -6,10 +6,10 @@ export interface ISplittedTime {
 export const splitTime = (
   startDate: Date,
   endDate: Date,
-  duration = 60 * 60 * 1000,
+  duration: number,
 ): ISplittedTime[] => {
   const startTimeMs = startDate.getTime();
-  const endTimeMs = endDate.getTime();
+  const endTimeMs = endDate.getTime() + 1;
 
   if (endTimeMs < startTimeMs || (endTimeMs - startTimeMs) % duration) {
     const err = new Error(`MOD_ERR`);
@@ -31,15 +31,3 @@ export const splitTime = (
 
   return slots;
 };
-
-// const dayDiff = (
-//   startDate: Date,
-//   endDate: Date,
-//   duration = 60 * 60 * 1000,
-// ): Object<{ startTime: Date; endTime: Date }> => {
-//   return { startTime: new Date(), endTime: new Date() };
-// };
-
-// const d1 = new Date('2018-07-17T13:00:00.000Z');
-// const d2 = new Date('2018-07-18T15:00:00.000Z');
-// console.log(splitTime(d1, d2));
