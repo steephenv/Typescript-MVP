f366fb747654f0a40784853043774b528b7b460e
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-07-20 16:43:30 +0530

    test for save availability added

--- tests/interview/save-availability.spec.ts
+++ tests/interview/save-availability.spec.ts
@@ -7,7 +7,16 @@ afterAll(() => mongooseConnectionPromise.then(() => mongoose.disconnect()));
 describe('Test for availability save', () => {
   test('creating slots', done => {
     supertest(app)
-      .get('/v1/interview/save-availability-calender')
+      .post('/v1/interview/save-availability-calender')
+      .send({
+        dateRange: {
+          startDate: 'Fri Jul 20 2018 09:58:32 GMT+0530 (India Standard Time)',
+          endDate: 'Sun Aug 05 2018 00:00:00 GMT+0530 (India Standard Time)',
+        },
+        workingDays: [1, 2, 5],
+        workingTime: { startTime: 9, endTime: 16 },
+        breakTime: { startTime: 1, endTime: 2 },
+      })
       .expect(200)
       .end((err, { body }) => {
         if (err) {
