a168ef609e505fb3026fc2f93201d11c0a97e3b8
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-07-24 11:57:42 +0530

    asset-category: frame

--- src/routes/assets/index.ts
+++ src/routes/assets/index.ts
@@ -6,9 +6,11 @@ import { errValidator } from '../../error-handler/error-validator';
 import { recordAssets } from './record-assets';
 import { listAssets } from './list-assets';
 import { listAssetCategory } from './list-asset-category';
+import { createAssetCategory } from './create-category';
 
 import { recordValidationChain } from './validators/record-assets.validation-chain';
 import { listValidationChain } from './validators/list-assets.validation-chain';
+import { createCategoryValidationChain } from './validators/create-category.validation-chain';
 
 export const assets = express.Router();
 
@@ -20,5 +22,11 @@ assets.get(
   listAssets,
 );
 assets.get('/category', listAssetCategory);
+assets.post(
+  '/category',
+  createCategoryValidationChain,
+  errValidator,
+  createAssetCategory,
+);
 
 assets.post('/record', recordValidationChain, errValidator, recordAssets);
