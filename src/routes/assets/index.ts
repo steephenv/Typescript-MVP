0a80848ea603ff39a9bc6005b8165504eb64c2cf
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-07-24 12:33:31 +0530

    (asset-sub-category): handling validation part and wireframe

--- src/routes/assets/index.ts
+++ src/routes/assets/index.ts
@@ -7,10 +7,12 @@ import { recordAssets } from './record-assets';
 import { listAssets } from './list-assets';
 import { listAssetCategory } from './list-asset-category';
 import { createAssetCategory } from './create-category';
+import { createAssetSubCategory } from './create-sub-category';
 
 import { recordValidationChain } from './validators/record-assets.validation-chain';
 import { listValidationChain } from './validators/list-assets.validation-chain';
 import { createCategoryValidationChain } from './validators/create-category.validation-chain';
+import { createSubCatValidationChain } from './validators/create-asset-sub-category.rule';
 
 export const assets = express.Router();
 
@@ -21,12 +23,19 @@ assets.get(
   queryIntParser(),
   listAssets,
 );
+
 assets.get('/category', listAssetCategory);
 assets.post(
   '/category',
   createCategoryValidationChain,
   errValidator,
   createAssetCategory,
+);
+assets.post(
+  '/sub-category',
+  createSubCatValidationChain,
+  errValidator,
+  createAssetSubCategory,
 );
 
 assets.post('/record', recordValidationChain, errValidator, recordAssets);
