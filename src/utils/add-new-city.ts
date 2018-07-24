cb183df41bdcf7dda25630befa0fe1e8ac953105
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-07-24 15:55:52 +0530

    stateiso changed to state name

--- src/utils/add-new-city.ts
+++ src/utils/add-new-city.ts
@@ -1,19 +1,33 @@
 import * as shortId from 'shortid';
 import { country } from '../models/Country';
 
-export const addNewCity = async (iso: string, cityName: string) => {
-  const extCountry = await country.find({
-    iso: new RegExp('^' + iso),
-    type: 'CI',
-    local_name: cityName,
-  });
+export const addNewCity = async (stateName: string, cityName: string) => {
+  const stateDetails: any = await country
+    .findOne({
+      type: 'RE',
+      local_name: stateName,
+    })
+    .exec();
+
+  if (!stateDetails) {
+    return;
+  }
+
+  const stateIso = stateDetails.iso;
+  const extCountry = await country
+    .find({
+      iso: new RegExp('^' + stateIso),
+      type: 'CI',
+      local_name: cityName,
+    })
+    .exec();
 
   if (!extCountry.length) {
     const cityCode = shortId.generate();
     const newCity = new country({
       type: 'CI',
       local_name: cityName,
-      iso: iso + '-' + cityCode,
+      iso: stateIso + '-' + cityCode,
     });
     await newCity.save();
   }
