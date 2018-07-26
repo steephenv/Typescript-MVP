f9e46c6bc3bc90ad1366a52da129df337901e83e
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-07-26 12:19:44 +0530

    rules for all category operations added

---
+++ src/routes/project/validators/delete-project-category-rule.ts
@@ -0,0 +1,25 @@
+import * as Joi from 'joi';
+import { RequestHandler } from 'express';
+
+// tslint:disable:variable-name
+const DeleteProjectCategorySchema = Joi.object().keys({
+  model: Joi.string().required(),
+  _id: Joi.string().required(),
+});
+
+export const deleteProjectCategoryRules: RequestHandler = (req, res, next) => {
+  Joi.validate(
+    req.query,
+    DeleteProjectCategorySchema,
+    { stripUnknown: true },
+    err => {
+      if (err) {
+        return res.status(422).send({
+          success: false,
+          msg: err,
+        });
+      }
+      next();
+    },
+  );
+};
