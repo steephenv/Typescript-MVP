2483c23f363942870aaea110517a33b08f00ad23
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-07-26 15:16:15 +0530

    asset list fuzzy search implemented

--- tests/assets/list-assets.spec.ts
+++ tests/assets/list-assets.spec.ts
@@ -4,12 +4,24 @@ import { app, mongoose, mongooseConnectionPromise } from '../../src/app';
 afterAll(() => mongooseConnectionPromise.then(() => mongoose.disconnect()));
 
 describe('testing asset listing', () => {
-  test('testing route', done => {
+  test('testing normal search', done => {
     supertest(app)
       .get('/v1/assets?fileName=mango')
       .set('X-Requested-With', 'XMLHttpRequest')
       .expect(200)
       .end(err => {
+        if (err) {
+          throw err;
+        }
+        done();
+      });
+  });
+  test.only('testing keyQuery search', done => {
+    supertest(app)
+      .get('/v1/assets?keyQuery=a&description=lorem ipsium doller sit')
+      .set('X-Requested-With', 'XMLHttpRequest')
+      .expect(200)
+      .end((err, { body }) => {
         if (err) {
           throw err;
         }
