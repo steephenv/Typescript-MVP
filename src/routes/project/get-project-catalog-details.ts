6feea6ba48aac0de90e097dc11a4b92c4c6f23b1
Steephen <steephen.varghese@cubettech.com>
2018-07-30 12:51:13 +0530

    updated api for getting project catalog and test file

---
+++ src/routes/project/get-project-catalog-details.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from 'express';
+import { Project } from '../../models/Project';
+import '../../models/ProjectCategory';
+import '../../models/ProjectSubCategory';
+import '../../models/IndustryLine';
+import '../../models/BusinessFunction';
+import '../../models/Business-sub-function';
+import {
+  RequestError,
+  RequestErrorType,
+} from '../../error-handler/RequestError';
+
+export const getProjects: RequestHandler = async (req, res, next) => {
+  try {
+    const projectList = await Project.find({})
+      .populate('category')
+      .populate('subCategory')
+      .populate('industryLine')
+      .populate('businessFunctions')
+      .populate('businessSubFunctions')
+      .exec();
+    return res.status(200).send({
+      success: true,
+      projects: projectList,
+    });
+  } catch (err) {
+    return next(new RequestError(RequestErrorType.INTERNAL_SERVER_ERROR, err));
+  }
+};
