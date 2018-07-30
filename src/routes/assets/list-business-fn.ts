991099624de3d32ef01eaaf61cc49f3c648d856b
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-07-30 12:58:34 +0530

    nested listing of subFns under Fns

--- src/routes/assets/list-business-fn.ts
+++ src/routes/assets/list-business-fn.ts
@@ -1,5 +1,8 @@
 import { RequestHandler } from 'express';
+import { Promise as BluePromise } from 'bluebird';
+
 import { BusinessFunction } from '../../models/Business-function';
+import { BusinessSubFunction } from '../../models/Business-sub-function';
 
 import {
   RequestError,
@@ -8,7 +11,20 @@ import {
 
 export const listBusinessFns: RequestHandler = async (req, res, next) => {
   try {
-    const businessFunctions = await BusinessFunction.find({}).exec();
+    const businessFunctions: any[] = await BusinessFunction.find({})
+      .lean()
+      .exec();
+
+    const bussSubFns = await BluePromise.map(businessFunctions, fn =>
+      BusinessSubFunction.find({
+        businessFunctionId: fn._id,
+      })
+        .lean()
+        .exec(),
+    );
+
+    businessFunctions.forEach((fn, i) => (fn.subFunctions = bussSubFns[i]));
+
     return res.status(200).send({
       success: true,
       businessFunctions,
