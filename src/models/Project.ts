0652c6412eca3842604532b74978450629693791
Steephen <steephen.varghese@cubettech.com>
2018-07-30 16:04:16 +0530

    changed model reference to  collections creted by vajahath

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -36,7 +36,7 @@ const projectSchema = new Schema({
   },
   industryLine: {
     type: Schema.Types.ObjectId,
-    ref: 'IndustryLine',
+    ref: 'Industry',
   },
   businessFunctions: {
     type: Schema.Types.ObjectId,
@@ -44,7 +44,7 @@ const projectSchema = new Schema({
   },
   businessSubFunctions: {
     type: Schema.Types.ObjectId,
-    ref: 'BusinessSubFunction',
+    ref: 'TempBusSubFunction',
   },
   projectStages: {
     type: String,
