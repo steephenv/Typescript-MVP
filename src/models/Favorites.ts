a5d2ff1898fab540212e5b44c75472a089277703
Steephen <steephen.varghese@cubettech.com>
2018-08-01 12:57:40 +0530

    added saving favourites functionality

---
+++ src/models/Favorites.ts
@@ -0,0 +1,25 @@
+/* tslint:disable:variable-name */
+import { model as mongooseModel, Schema } from 'mongoose';
+
+const favoritesSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+  },
+  type: {
+    type: String,
+  },
+  collectionType: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+export const Favorites = mongooseModel('Favorites', favoritesSchema);
