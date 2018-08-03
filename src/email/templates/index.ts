9c5e0d69cc5ae712ff03b453638bdf84c83a19d7
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-08-03 15:01:52 +0530

    email templates added

--- src/email/templates/index.ts
+++ src/email/templates/index.ts
@@ -5,4 +5,20 @@ export class EmailTemplates {
   public static SAMPLE = readFileSync(
     pathJoin(__dirname, 'sample.ejs'),
   ).toString();
+
+  public static RESET_PASSWORD = readFileSync(
+    pathJoin(__dirname, 'reset-password.ejs'),
+  ).toString();
+
+  public static EMAIL_RECOVERY = readFileSync(
+    pathJoin(__dirname, 'email-recovery.ejs'),
+  ).toString();
+
+  public static CONFIRM_REGISTRATION = readFileSync(
+    pathJoin(__dirname, 'confirm-registration.ejs'),
+  ).toString();
+
+  public static ROLE_ACCEPT = readFileSync(
+    pathJoin(__dirname, 'user-role-accept.ejs'),
+  ).toString();
 }
