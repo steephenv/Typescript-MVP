02dee697a96f26612b995ebc2b583d81c100b7e1
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-08-06 14:44:16 +0530

    fixing format err issue

--- src/graphql-compiler/formatError.ts
+++ src/graphql-compiler/formatError.ts
@@ -1,6 +1,32 @@
 import { GQLErr } from '../error-handler/GQL-Error';
 
 export function formatError(error: GQLErr) {
-  console.log(error);
-  return error;
+  const origErr: any = error.originalError;
+
+  const errToDisplay: { [key: string]: any } = {
+    locations: error.locations,
+    path: error.path,
+  };
+
+  if (error.message === origErr.message) {
+    errToDisplay.message = error.message;
+  } else {
+    errToDisplay.message = error.message;
+    errToDisplay.moreMessage = origErr.message;
+  }
+
+  if (error.extensions) {
+    errToDisplay.extensions = error.extensions;
+  }
+  if (origErr.err) {
+    errToDisplay.errCode = origErr.err;
+  }
+  if (origErr.details) {
+    errToDisplay.details = origErr.details;
+  }
+  if (origErr.statusCode) {
+    errToDisplay.statusCode = origErr.statusCode;
+  }
+
+  return errToDisplay;
 }
