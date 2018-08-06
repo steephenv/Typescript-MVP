0e8abe44c4ae94c5d9791ae29e44d7a64d7eabdd
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-08-06 16:28:38 +0530

    more err msgs

--- src/graphql-compiler/formatError.ts
+++ src/graphql-compiler/formatError.ts
@@ -4,29 +4,31 @@ export function formatError(error: GQLErr) {
   const origErr: any = error.originalError;
 
   const errToDisplay: { [key: string]: any } = {
+    message: error.message,
     locations: error.locations,
     path: error.path,
   };
 
-  if (error.message === origErr.message) {
-    errToDisplay.message = error.message;
-  } else {
-    errToDisplay.message = error.message;
-    errToDisplay.moreMessage = origErr.message;
-  }
-
   if (error.extensions) {
     errToDisplay.extensions = error.extensions;
   }
-  if (origErr.err) {
-    errToDisplay.errCode = origErr.err;
-  }
-  if (origErr.details) {
-    errToDisplay.details = origErr.details;
-  }
-  if (origErr.statusCode) {
-    errToDisplay.statusCode = origErr.statusCode;
-  }
 
+  if (origErr) {
+    if (error.message === origErr.message) {
+      errToDisplay.message = error.message;
+    } else {
+      errToDisplay.message = error.message;
+      errToDisplay.moreMessage = origErr.message;
+    }
+    if (origErr.err) {
+      errToDisplay.errCode = origErr.err;
+    }
+    if (origErr.details) {
+      errToDisplay.details = origErr.details;
+    }
+    if (origErr.statusCode) {
+      errToDisplay.statusCode = origErr.statusCode;
+    }
+  }
   return errToDisplay;
 }
