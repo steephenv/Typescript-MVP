495c7c56e2bbb6fb5be05b3ee077efa22aebdf2e
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-08-06 15:11:40 +0530

    ui improvements

--- src/graphql/humans/getHumans.gqr.ts
+++ src/graphql/humans/getHumans.gqr.ts
@@ -5,9 +5,5 @@ export const querySchema = `getHumans: String`;
 export const resolver = { getHumans };
 
 function getHumans() {
-  if (Math.floor(Math.random() * 10) < 5) {
-    return `We're humans :) (<5)`;
-  } else {
-    throw new GQLErr(GQLErrType.BAD_REQUEST, 'details----');
-  }
+  return `We're humans :) (<5)`;
 }
