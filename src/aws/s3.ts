a0e6e93feb69f82e1a3526d80b13eeaadadc3c61
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-08-10 16:51:11 +0530

    removes duplicates

--- src/aws/s3.ts
+++ src/aws/s3.ts
@@ -4,6 +4,4 @@ export const s3 = new AWS.S3({
   apiVersion: '2010-12-01',
   signatureVersion: 'v4',
   region: 'eu-central-1',
-  accessKeyId: 'AKIAINNSIVNKKEZOX7BA',
-  secretAccessKey: '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',
 });
