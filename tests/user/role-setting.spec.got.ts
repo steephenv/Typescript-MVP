a8bb8179998a49f01ee65c25d08d6896c8c0f6ee
Steephen <steephen.varghese@cubettech.com>
2018-08-10 14:21:54 +0530

    changed User test files from jest to got structure

---
+++ tests/user/role-setting.spec.got.ts
@@ -0,0 +1,54 @@
+import * as got from 'got';
+
+let token = '';
+let newUserId = '';
+
+beforeAll(done => {
+  got('http://localhost:7000/v1/auth/login', {
+    method: 'POST',
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+    json: true,
+    body: {
+      username: 'stark@marvel.com',
+      password: 'password',
+    },
+  })
+    .then(({ body }: any) => {
+      token = body.accessToken;
+      newUserId = body.data._id;
+      done();
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+describe('Test for signup functionality  ===> ', () => {
+  it(
+    'Registration Functionality',
+    done => {
+      got('http://localhost:7000/v1/auth/role-setting', {
+        method: 'POST',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+          Authorization: `Bearer ${token}`,
+        },
+        json: true,
+        body: {
+          userId: newUserId,
+          role: 'Consultant',
+          isApproved: true,
+          comment: 'fdsgf',
+          interviewId: '5b506d48e618c7361b6a3977',
+        },
+      })
+        .then(() => done())
+        .catch(err => {
+          throw err;
+        });
+    },
+    15000,
+  );
+});
