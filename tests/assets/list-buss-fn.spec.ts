9df0d527a97eb6ebcb943dacdfebefa4fd657df3
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-08-29 15:10:11 +0530

    docs and tests for updated bus-fn listing api

--- tests/assets/list-buss-fn.spec.ts
+++ tests/assets/list-buss-fn.spec.ts
@@ -17,4 +17,20 @@ describe('testing asset listing', () => {
         throw err;
       });
   });
+  test('testing route with limit', done => {
+    got('http://localhost:7000/v1/assets/business-functions?_limit=1', {
+      method: 'GET',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      // json: true,
+      // body: {
+      //   name: 'Acinonyx',
+      // },
+    })
+      .then(() => done())
+      .catch(err => {
+        throw err;
+      });
+  });
 });
