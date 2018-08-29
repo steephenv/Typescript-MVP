331dda4b99f97fe95230cdb4e958d3d083e6ea9e
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-08-29 15:09:35 +0530

    adds count, limit, skip for list buss fn

--- src/routes/assets/list-business-fn.ts
+++ src/routes/assets/list-business-fn.ts
@@ -11,11 +11,26 @@ import {
 
 export const listBusinessFns: RequestHandler = async (req, res, next) => {
   try {
+    const limit = req.query._limit || 50;
+    const skip = req.query._skip || 0;
+
+    // quick validation
+    if (isNaN(limit) || isNaN(skip)) {
+      return next(
+        new RequestError(
+          RequestErrorType.UNPROCESSABLE_ENTITY,
+          'limit and skip should be numbers',
+        ),
+      );
+    }
+
     const businessFunctions: any[] = await BusinessFunction.find({})
+      .limit(limit)
+      .skip(skip)
       .lean()
       .exec();
 
-    const bussSubFns = await BluePromise.map(businessFunctions, fn =>
+    const bussSubFnsP = BluePromise.map(businessFunctions, fn =>
       BusinessSubFunction.find({
         businessFunctionId: fn._id,
       })
@@ -23,11 +38,20 @@ export const listBusinessFns: RequestHandler = async (req, res, next) => {
         .exec(),
     );
 
+    const totalBusinessFunctionsCountP = BusinessFunction.count({}).exec();
+
+    const [bussSubFns, totalBusinessFunctionsCount] = await BluePromise.all([
+      bussSubFnsP,
+      totalBusinessFunctionsCountP,
+    ]);
+
+    // attach stuffs
     businessFunctions.forEach((fn, i) => (fn.subFunctions = bussSubFns[i]));
 
     return res.status(200).send({
       success: true,
       businessFunctions,
+      totalBusinessFunctionsCount,
     });
   } catch (err) {
     return next(new RequestError(RequestErrorType.INTERNAL_SERVER_ERROR, err));
