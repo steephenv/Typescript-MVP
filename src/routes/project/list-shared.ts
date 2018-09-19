9be5e0c378c8591fb56b618f557e205df36a5555
Steephen <steephen.varghese@cubettech.com>
2018-09-19 12:43:40 +0530

    updated list shared api and project pdf ejs

--- src/routes/project/list-shared.ts
+++ src/routes/project/list-shared.ts
@@ -16,6 +16,11 @@ import { Share } from '../../models/share';
 
 export const listShared: RequestHandler = async (req, res, next) => {
   try {
+    const dataCount = await Share.count({
+      userId: res.locals.user.userId,
+    })
+      .distinct('projectId')
+      .exec();
     const sharedIds = await Share.find({
       userId: res.locals.user.userId,
     })
@@ -24,11 +29,14 @@ export const listShared: RequestHandler = async (req, res, next) => {
     const projectDetails = await Project.find({
       _id: { $in: sharedIds },
     })
+      .limit(req.query.limit)
+      .skip(req.query.skip)
       .select('_id projectTitle picture currentSituation')
       .exec();
     return res.status(200).send({
       success: true,
       projects: projectDetails,
+      count: dataCount,
     });
   } catch (err) {
     return next(new RequestError(RequestErrorType.INTERNAL_SERVER_ERROR, err));
