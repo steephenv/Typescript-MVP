66d2b20f6ff86a30fc26c7de1d8aa9cd95dd59bd
Vajahath Ahmed <vajahath.ahmed@cubettech.com>
2018-09-20 12:39:47 +0530

    schedule interview email resp ack

--- src/routes/interview/schedule-interview.ts
+++ src/routes/interview/schedule-interview.ts
@@ -156,7 +156,9 @@ export const scheduleInterview: RequestHandler = async (req, res, next) => {
     return res.status(201).send({
       success: true,
       msg: messages.interviewScheduled.ENG,
-      mailSentTo: admin.email + ', ' + userDetails.email,
+      mailSentTo: `${admin ? admin.email : 'null'}, ${
+        userDetails ? userDetails.email : 'null'
+      }`,
     });
   } catch (err) {
     return next(new RequestError(RequestErrorType.INTERNAL_SERVER_ERROR, err));
