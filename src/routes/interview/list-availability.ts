84bcc3e6405667e234525c6d10e55d2f0a661d1a
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-09-28 12:48:31 +0530

    list availability date range start change

--- src/routes/interview/list-availability.ts
+++ src/routes/interview/list-availability.ts
@@ -1,17 +1,30 @@
 import { RequestHandler } from 'express';
+import * as moment from 'moment-timezone';
+
 import {
   RequestError,
   RequestErrorType,
 } from '../../error-handler/RequestError';
+
 import { InterviewAvailabilityCalender } from '../../models/InterviewAvailabilityCalender';
 
 export const listBPMAvailability: RequestHandler = async (req, res, next) => {
   try {
-    const dateInit = new Date(new Date().setDate(new Date().getDate() + 2));
+    const timezone = req.query.timezone || 'Europe/Berlin';
+    const today = moment(new Date()).tz(timezone);
+    const todayInWeek = today.format('dddd');
+
+    let offset = 2;
+    if (todayInWeek === 'Friday') {
+      offset = 4;
+    }
+    const dateInit = new Date(
+      new Date().setUTCDate(new Date().getUTCDate() + offset),
+    );
     const gettingDate = req.query.date ? new Date(req.query.date) : dateInit;
     const forward = req.query.forward ? req.query.forward : 'true';
-    const givenStartTime = new Date(gettingDate.setHours(23, 59, 59, 999));
-    const givenEndTime = new Date(gettingDate.setHours(0, 0, 0, 0));
+    const givenStartTime = new Date(gettingDate.setUTCHours(23, 59, 59, 999));
+    const givenEndTime = new Date(gettingDate.setUTCHours(0, 0, 0, 0));
 
     let timeQuery = {};
     let sortVariable = 1;
