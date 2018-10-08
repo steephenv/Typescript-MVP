65e5b767f35449e138cfd3bfbbba96838ec13a7a
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-10-08 17:48:24 +0530

    questionnare model added

---
+++ src/models/Questionnaire.ts
@@ -0,0 +1,44 @@
+/* tslint:disable:variable-name */
+import { model as mongooseModel, Schema } from 'mongoose';
+
+export const description = 'Stores question and answers';
+export const definitions = {
+  updatedAt: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+  },
+  projectId: {
+    type: Schema.Types.ObjectId,
+    ref: 'ProjectRequest',
+  },
+  type: {
+    type: String,
+  },
+  askedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  question: {
+    type: String,
+  },
+  answer: [
+    {
+      text: { type: String },
+      author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      answeredAt: {
+        type: Date,
+      },
+    },
+  ],
+};
+const QuestionnaireSchema: Schema = new Schema(definitions);
+
+export const Questionnaire = mongooseModel(
+  'Questionnaire',
+  QuestionnaireSchema,
+);
