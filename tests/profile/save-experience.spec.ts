0c9c8b375c90ad969538b18c7993393808b56b1b
lakshmipriyamukundan <lakahmipriya.m@cubettech.com>
2018-10-16 17:56:44 +0530

    save profile - stripe unknown change

--- tests/profile/save-experience.spec.ts
+++ tests/profile/save-experience.spec.ts
@@ -1,4 +1,32 @@
 import * as got from 'got';
+// import * as Joi from 'joi';
+
+// const testData = [
+//   {
+//     name: 'hello',
+//     hello: 'hai',
+//   },
+//   {
+//     hello: 'hai',
+//     name: 'hell',
+//   },
+// ];
+
+// const objSchema: any = Joi.object().keys({
+//   name: Joi.string().required(),
+// });
+
+// const objArraySchema = Joi.array().items(objSchema);
+
+// Joi.validate(testData, objArraySchema, {}, err => {
+//   console.log(err);
+//   console.log('.............', testData);
+//   if (err) {
+//     console.log(err);
+//   }
+//   return;
+//   // next();
+// });
 
 let token = '';
 let newUserId: string;
@@ -47,13 +75,12 @@ describe('Test for saving experience data ', () => {
             locationCountry: 'ddd',
             locationCity: 'esdftg',
             jobTitle: 'esdftg',
-            stateIso: 'yuuyuy',
             mainResponsibility: 'sfs',
             peopleManagementResponsibility: 'fsdf',
             managedTeamSize: 100,
             budgetResponsibility: 152,
             locationState: 'sdfbsh',
-            managementType: 'dsfsdgfs',
+            managementType: '5bc08938ac096344d60a9e6c',
           },
         ],
         projects: [
@@ -63,11 +90,10 @@ describe('Test for saving experience data ', () => {
             engagementAs: 'sdf',
             clientsCompanyName: 'feew',
             companyIndustryLine: 'sdfs',
+            businessFunction: 'sdefg',
             clientsCompanySize: '222',
             locationCountry: 'fsedf',
             locationCity: 'qqqq',
-            stateIso: 'yuuyuy',
-            businessFunction: 'fsdefs',
             projectGoal: 'sdgfsd',
             projectName: 'sdgfsd',
             projectDuration: 'sedftge',
@@ -89,4 +115,64 @@ describe('Test for saving experience data ', () => {
         throw err;
       });
   });
+  // test('Saving Experience details api - bad data', done => {
+  //   got(`http://localhost:7000/v1/profile/save-experience`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'X-Requested-With': 'XMLHttpRequest',
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //     json: true,
+  //     body: {
+  //       experiences: [
+  //         {
+  //           businessFunction: 'sdefg',
+  //           companyName: 'sfdse',
+  //           companyIndustryLine: 'dsgdg',
+  //           companySize: '100',
+  //           locationCountry: 'ddd',
+  //           locationCity: 'esdftg',
+  //           jobTitle: 'esdftg',
+  //           stateIso: 'yuuyuy',
+  //           mainResponsibility: 'sfs',
+  //           peopleManagementResponsibility: 'fsdf',
+  //           managedTeamSize: 100,
+  //           budgetResponsibility: 152,
+  //           locationState: 'sdfbsh',
+  //           managementType: 'dsfsdgfs',
+  //         },
+  //       ],
+  //       projects: [
+  //         {
+  //           engagementFrom: 'dffgd',
+  //           engagementTo: 'dfds',
+  //           engagementAs: 'sdf',
+  //           clientsCompanyName: 'feew',
+  //           companyIndustryLine: 'sdfs',
+  //           clientsCompanySize: '222',
+  //           locationCountry: 'fsedf',
+  //           locationCity: 'qqqq',
+  //           stateIso: 'yuuyuy',
+  //           projectGoal: 'sdgfsd',
+  //           projectName: 'sdgfsd',
+  //           projectDuration: 'sedftge',
+  //           projectSize: '111',
+  //           projectComplexity: 'fegtf',
+  //           projectRegionalReach: 'sdfsdfqq',
+  //           role: 'safsd',
+  //           projectTeamSize: 111,
+  //           budgetResponsibility: 2315,
+  //           mainResults: 'fdsds',
+  //           applicableToOtherCompanies: 'dsfgdg',
+  //           locationState: 'sdbcvsgavc',
+  //         },
+  //       ],
+  //     },
+  //   })
+  //     // .then(() => done())
+  //     .catch(err => {
+  //       // throw err;
+  //       done(err);
+  //     });
+  // });
 });
