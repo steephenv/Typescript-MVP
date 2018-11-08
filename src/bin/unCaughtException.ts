6b1470c842bb8f1457641449857e11b746cb3121
Steephen <steephen.varghese@cubettech.com>
2018-11-08 15:01:29 +0530

    uncaught exception file error printing added

--- src/bin/unCaughtException.ts
+++ src/bin/unCaughtException.ts
@@ -16,6 +16,14 @@ const ERR_EMAIL: string = getConfig('mails.errMails');
 
 // handle uncaught exception
 process.on('uncaughtException', async error => {
+  // tslint:disable-next-line:no-console
+  console.log('########################################\n');
+  // tslint:disable-next-line:no-console
+  console.log(error);
+  // tslint:disable-next-line:no-console
+  console.trace(error);
+  // tslint:disable-next-line:no-console
+  console.log('\n########################################\n');
   try {
     if (
       process.env.NODE_ENV !== 'development' &&
